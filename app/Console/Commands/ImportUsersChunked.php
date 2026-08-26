aa3f0fe2d4de64023de0fcc453d4183c8b56c4c8
hansakoch <hansakoch@users.noreply.github.com>
2026-08-26 18:28:31 +0000

    Fix chunked import - hardcoded chunk list

--- app/Console/Commands/ImportUsersChunked.php
+++ app/Console/Commands/ImportUsersChunked.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\DB;
 class ImportUsersChunked extends Command
 {
     protected $signature = 'ffm:import-users-chunked';
-    protected $description = 'Import users in chunks from TMD';
+    protected $description = 'Import users in chunks';
 
     public function handle()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         DB::statement('SET SQL_MODE = ""');
         
-        $chunks = range('aa', 'az');
+        $chunks = explode(',', 'aa,ab,ac,ad,ae,af,ag,ah,ai,aj,ak,al,am,an,ao,ap,aq,ar,as,at,au,av,aw,ax,ay,az,ba,bb,bc,bd,be,bf,bg,bh,bi,bj');
         $imported = 0;
         
         foreach ($chunks as $suffix) {
@@ -31,25 +31,9 @@ class ImportUsersChunked extends Command
             }
         }
         
-        // Also try ba-bz
-        foreach (range('ba', 'bz') as $suffix) {
-            $url = "https://fansfollow.me/ffm_table_data/users_chunk_{$suffix}";
-            $sql = @file_get_contents($url);
-            if ($sql && strlen($sql) > 100) {
-                $sql = str_replace('INSERT INTO', 'INSERT IGNORE INTO', $sql);
-                try {
-                    DB::unprepared($sql);
-                    $imported++;
-                    $this->line("OK: chunk_{$suffix}");
-                } catch (\Exception $e) {
-                    $this->warn("ERR: chunk_{$suffix} - " . substr($e->getMessage(), 0, 60));
-                }
-            }
-        }
-        
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
         $count = DB::select('SELECT COUNT(*) as c FROM users');
-        $this->info("Done: {$imported} chunks, " . ($count[0]->c ?? 0) . " users total");
+        $this->info("Done: {$imported} chunks, " . ($count[0]->c ?? 0) . " users");
         return 0;
     }
 }
